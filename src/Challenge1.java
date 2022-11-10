f6561fee97803044abd720124bcc0fad986ecc8e
Mariana Zapata <marianai.zapatar@gmail.com>
2022-11-10 12:28:55 -0500

    new project added with first feature - user input

---
+++ src/Challenge1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Challenge1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Please type 'Type A' or 'Type B'");
+		
+		String type = sc.nextLine();
+		
+		System.out.println("The type is " + type);
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
