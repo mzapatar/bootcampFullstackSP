855005ba6b50e43d617df173c788d4687a655b74
Mariana Zapata <marianai.zapatar@gmail.com>
2022-11-11 01:13:41 -0500

    added random number generator feature

--- src/Challenge1.java
+++ src/Challenge1.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Challenge1 {
 
@@ -6,17 +7,42 @@ public class Challenge1 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Please type 'Type A' or 'Type B'");
+		System.out.println("Please type 'Type A' or 'Type B': ");
 		
 		String type = sc.nextLine();
 		
-		System.out.println("The type is " + type);
+		System.out.println("The requested type is " + type);
 		
+		String ten_digit_number = Random10DigNumber(type);
 		
-		
+		System.out.println("The generated number is " + ten_digit_number);
 		
 		sc.close();
 
+	}
+	
+	//Method to generate a 10-digit pseudo-random number 
+	public static String Random10DigNumber(String type) {
+		
+		Random rand = new Random();
+		  
+		Integer eight_digit_number = rand.nextInt(100000000);
+		
+		String str_eight_digit_number = eight_digit_number.toString();
+		
+		String ten_digit_number = "";
+      
+		boolean check = type.toLowerCase().contains("a");
+      
+		if (check == true) {
+			ten_digit_number = ten_digit_number + "54" + str_eight_digit_number;
+		} else {
+			ten_digit_number = ten_digit_number + "08" + str_eight_digit_number;
+		}
+		
+		return ten_digit_number;
+		
+		
 	}
 
 }
