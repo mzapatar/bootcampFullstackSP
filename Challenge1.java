c8774bd228ec566c5b71d3774e2f7d4f920caf80
mzapatar <95182880+mzapatar@users.noreply.github.com>
2022-11-14 17:10:35 -0500

    Add files via upload

---
+++ Challenge1.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Challenge1 {
+
+	public static void main(String[] args) {
+		
+		//User input to use Random10DigNumber method (see method below main)
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Please type 'Type A' or 'Type B': ");
+		
+		String type = sc.nextLine();
+		
+		String ten_digit_number = Random10DigNumber(type);
+		
+		while (ten_digit_number == null) {
+			System.out.println("The requested type '" + type + "' is not valid. Please type 'Type A' or 'Type B': ");
+			type = sc.nextLine();
+			ten_digit_number = Random10DigNumber(type);
+		} 
+		
+		System.out.println("The requested type is '" + type + "'.");
+		
+		System.out.println("The generated number is " + ten_digit_number);
+		
+		//List and String to use ListCheck method (see method below main). The program will check if the number generated via Random10DigNumber is contained in a list of 4 ten-digit numbers.
+		String [] numbers = {"5469660026","5400264588","0845961536","0810000651"};
+		
+		List<String> number_list = Arrays.asList(numbers);
+		
+		boolean check = ListCheck(ten_digit_number,number_list);
+		
+		System.out.println("Is the number available?: " + check);
+		
+		if (check == true) {
+			System.out.println(ten_digit_number + " has been successfully assigned. ");
+		} else {
+			System.out.println(ten_digit_number + " is unavailable and cannot be assigned. ");
+		}
+		
+		sc.close();
+
+	}
+	
+	//Method to generate two types of 10-digit pseudo-random numbers (Type A starts in 54 and Type B starts in 08. Any other type will return null.) 
+	public static String Random10DigNumber(String type) {
+		
+		Random rand = new Random();
+		  
+		Integer random_number = rand.nextInt(100000000);
+		
+		String str_random_number = random_number.toString();
+		
+		int needed_width = 8;
+		
+		String eight_digit_number = String.format("%0" + needed_width + "d", Integer.valueOf(str_random_number));
+		
+		String ten_digit_number = "";
+      
+		boolean checka = type.toLowerCase().contains("a");
+		
+		boolean checkb = type.toLowerCase().contains("b");
+      
+		if (checka == true) {
+			ten_digit_number = ten_digit_number + "54" + eight_digit_number;
+		} else {
+			if (checkb == true) {
+				ten_digit_number = ten_digit_number + "08" + eight_digit_number;
+			} else {
+				return null;
+			}
+		}
+		
+		return ten_digit_number;
+		
+		
+	}
+	
+	//Method to check if given String of text is contained in given list. If the number is contained, the method will return false
+	public static boolean ListCheck(String str, List<String> list) {
+		
+		boolean check = list.contains(str);
+		
+		boolean check_converted = (check == true)?false:true;
+		
+		return check_converted;
+	}
+
+}
